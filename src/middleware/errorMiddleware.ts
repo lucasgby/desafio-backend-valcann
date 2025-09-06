e2f081a24f14b31f92f158e9253eb6bf870c3afe
lugyll <lucas.gabdev@gmail.com>
2025-09-06 11:04:32 -0300

    Error Middleware

---
+++ src/middleware/errorMiddleware.ts
@@ -0,0 +1,31 @@
+import type { Request, Response, NextFunction } from "express";
+import { ZodError } from "zod";
+
+export function errorMiddleware(
+  err: any,
+  req: Request,
+  res: Response,
+  next: NextFunction
+) {
+  console.error(err);
+
+  if (err instanceof ZodError) {
+    const messages = err.issues.map((e) => e.message);
+
+    return res.status(400).json({
+      error: {
+        code: "VALIDATION_ERROR",
+        message: "Invalid request data",
+        details: messages,
+      },
+    });
+  }
+
+  const status = err.status || 500;
+  const message = err.message || "Internal Server Error";
+
+  res.status(status).json({
+    exists: false,
+    message,
+  });
+}
